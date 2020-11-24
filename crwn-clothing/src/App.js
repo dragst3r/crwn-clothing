9bc8acf02ee01aedde757829fe5ebdaa5d352285
dragst3r <zummi2@gmail.com>
2020-11-24 19:02:28 +0100

    msg

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import './pages/homepage/homepage.styles.scss'
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage =() => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch> 
     </div>
   );
