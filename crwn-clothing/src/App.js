5ff187ad8bee37d176c02b98bad6c9856ea4c8fd
dragst3r <zummi2@gmail.com>
2020-11-26 19:33:53 +0100

    header component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,12 +3,14 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
