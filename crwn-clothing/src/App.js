36843c4b1b81762a6f839a5afcdb1ec2e1fb4915
dragst3r <zummi2@gmail.com>
2020-11-29 18:27:09 +0100

    redux

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import './App.css';
@@ -35,7 +35,7 @@ class App extends React.Component {
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
-  }
+  };
   render() {
     return (
       <div>
@@ -43,15 +43,17 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
-
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
