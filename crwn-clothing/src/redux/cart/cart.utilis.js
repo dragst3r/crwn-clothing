22304becf0f8bba3c144bce5edc8484fbbdaec1b
dragst3r <zummi2@gmail.com>
2020-12-05 18:56:22 +0100

    m

--- crwn-clothing/src/redux/cart/cart.utilis.js
+++ crwn-clothing/src/redux/cart/cart.utilis.js
@@ -11,4 +11,21 @@ export const addItemToCart = (cartItems, cartItemtoAdd) => {
 
     return [...cartItems,{...cartItemtoAdd, quantity: 1}]
 
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const exisingCartItem = cartItems.find(
+        cartItem => cartItem.id ===  cartItemToRemove.id
+    )
+
+    if(exisingCartItem.quantity ===1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem =>
+        cartItem.id === cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity -1}
+        : cartItem
+    )
 }
